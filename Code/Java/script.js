dda67e0dd627bfdcfb0b3a371a676c2eab3701b8
dangtrong7466 <dngtrog671@gmail.com>
2026-07-19 01:08:12 +0700

    change

--- Code/Java/script.js
+++ Code/Java/script.js
@@ -5,14 +5,11 @@ function upDate(previewPic){
     var displayDiv = document.getElementById("image");
     displayDiv.innerHTML = previewPic.alt;
     displayDiv.style.backgroundImage = "url('" + previewPic.src + "')";
-    displayDiv.style.lineHeight = "normal";
-    displayDiv.style.paddingTop = "160px";
 }
 
 function undo(){
+    console.log("Sự kiện undo đã kích hoạt!");
     var displayDiv = document.getElementById("image");
     displayDiv.style.backgroundImage = "url('')";
     displayDiv.innerHTML = "Di chuột qua một hình ảnh bên dưới để hiển thị ở đây.";
-    displayDiv.style.lineHeight = "350px";
-    displayDiv.style.paddingTop = "0px";
 }
